832ff5538b51dacb2bec09356a0b86c10d5fddd7
Ali Anwar <greenywish@gmail.com>
2024-01-03 18:49:04 +0530

    Add Hover Animation on Button

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center h-screen">
-    <button class="p-16 text-3xl border border-red-500 bg-red-500 m-2 rounded" wire:click="decrement">-</button>
+    <button class="p-16 text-3xl border border-red-500 bg-red-500 m-2 rounded hover:scale-105 hover:border-transparent hover:border-2 duration-300 ease-in-out"   wire:click="decrement">-</button>
     <button class="p-16 text-3xl border border-gray-500 bg-gray-500 m-2 rounded" wire:model="count">{{$count}}</button>
-    <button class="p-16 text-3xl border border-blue-500 bg-blue-500 m-2 rounded" wire:click="increment">+</button>
+    <button class="p-16 text-3xl border border-blue-500 bg-blue-500 m-2 rounded hover:scale-105 hover:border-transparent hover:border-2 duration-300 ease-in-out" wire:click="increment">+</button>
 </div>
